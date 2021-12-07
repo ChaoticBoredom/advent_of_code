eb4d251fd035ee1b25275b9df88621688bb36c23
Susan Wright <wright1@ualberta.ca>
2021-12-06 22:24:45 -0700

    Solution for day 7

---
+++ 2021/07/sol.rb
@@ -0,0 +1,21 @@
+require_relative "../../aoc_input"
+
+input = get_input(2021, 7).split(",").map(&:to_i).tally
+
+def calc_fuel(input)
+  fuel_cost = Hash.new(0)
+
+  input.keys.max.times do |k|
+    fuel_used = 0
+    input.each_key do |k2|
+      fuel = (k - k2).abs
+      fuel_used += (yield(fuel) * input[k2])
+    end
+    fuel_cost[k] = fuel_used
+  end
+
+  fuel_cost.min_by { |_, v| v }.last
+end
+
+puts calc_fuel(input) { |fuel| fuel }
+puts calc_fuel(input) { |fuel| 1.upto(fuel).sum }
