989d3368bb21f177b9569091cd4d6bea6852e8a1
Susan Wright <wright1@ualberta.ca>
2021-12-16 16:10:13 -0700

    2015 revisted! Day 1

---
+++ 2015/01/sol.rb
@@ -0,0 +1,16 @@
+require_relative "../../aoc_input"
+
+input = get_input(2015, 1).split("")
+
+floor = 0
+basement = nil
+
+input.each.with_index(1) do |char, idx|
+  binding.pry
+  floor += 1 if char == "("
+  floor -= 1 if char == ")"
+  basement = idx if floor.negative? && basement.nil?
+end
+
+puts floor
+puts basement
