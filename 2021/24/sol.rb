48850a70633f3032172220fada6ef47f0955720e
Susan Wright <wright1@ualberta.ca>
2021-12-24 15:38:37 -0700

    Programmatic solution for day 24

---
+++ 2021/24/sol.rb
@@ -0,0 +1,77 @@
+require_relative "../../aoc_input"
+
+program = get_input(2021, 24).split("\n").each_slice(18).to_a
+
+def run_program(program, input)
+  w = x = y = z = 0
+  program.each do |line|
+    w = input.pop
+    x = z
+    x %= 26
+    z /= line[4].match(/\d+/)[0].to_i
+    x += line[5].match(/-?\d+/)[0].to_i
+    x = x == w ? 1 : 0
+    x = x.zero? ? 1 : 0
+    y = 25
+    y *= x
+    y += 1
+    z *= y
+    y = w
+    y += line[15].match(/\d+/)[0].to_i
+    y *= x
+    z += y
+  end
+  z
+end
+
+def find_rules(program)
+  uniqs = program.map.with_index do |line, idx|
+    [
+      idx,
+      line[5].match(/-?\d+/)[0].to_i,
+      line[15].match(/\d+/)[0].to_i,
+    ]
+  end
+  stack = []
+  uniqs.map do |idx, x, _|
+    stack << idx if x.positive?
+    next if x.positive?
+
+    prev = stack.pop
+    "num[#{idx}] = num[#{prev}] + #{uniqs[prev][2] + x}"
+  end.compact
+end
+
+def find_max(program)
+  rules = find_rules(program)
+  num = [9] * 14
+  rules.each do |x|
+    res = eval(x)
+    while res > 9
+      val = x.scan(/num\[\d+\]/)[1]
+      eval("#{val} -= 1")
+      res = eval(x)
+    end
+  end
+  num.join.to_i
+end
+
+def find_min(program)
+  rules = find_rules(program)
+  num = [1] * 14
+  rules.each do |x|
+    res = eval(x)
+    while res < 1
+      val = x.scan(/num\[\d+\]/)[1]
+      eval("#{val} += 1")
+      res = eval(x)
+    end
+  end
+  num.join.to_i
+end
+
+max = find_max(program)
+min = find_min(program)
+
+puts max if run_program(program, max.digits).zero?
+puts min if run_program(program, min.digits).zero?
