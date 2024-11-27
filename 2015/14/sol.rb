b9e2d3911773fb1e8f2681bd9989cbbf3c564d3a
Susan Wright <wright1@ualberta.ca>
2024-11-27 02:19:31 -0700

    Solution for Day 14, 2015

---
+++ 2015/14/sol.rb
@@ -0,0 +1,44 @@
+require_relative "../../aoc_input"
+
+class Reindeer
+  attr_accessor :fly_time, :fly_speed, :rest_time, :current_distance
+
+  def initialize
+    @fly_time = 0
+    @fly_speed = 0
+    @rest_time = 0
+    @current_distance = 0
+  end
+end
+
+input = get_input(2015, 14).split("\n")
+
+def parse_input(input)
+  input.map { |l| /(\w+) .* (\d+) .* (\d+) .* (\d+) seconds./.match(l).captures }.
+    to_h { |k, s, ft, rt| [k, [s.to_i, ft.to_i, rt.to_i]] }
+end
+
+def run_old_school_race(time, deer)
+  deer.map do |k, v|
+    speed, fly_time, rest_time = v
+    movement = speed * fly_time
+    cycles = time / (fly_time + rest_time)
+    remaining = time % (fly_time + rest_time)
+    distance = cycles * movement
+    distance += remaining >= fly_time ? movement : speed * remaining
+    [k, distance]
+  end
+end
+
+def run_new_race(time, deer)
+  deer_tally = deer.to_h { |k, _| [k, 0] }
+  (1..time).each do |i|
+    point_getter = run_old_school_race(i, deer).group_by { |v| v[1] }.max[1].map { |v| v[0] }
+    point_getter.each { |pg| deer_tally[pg] += 1 }
+  end
+  deer_tally
+end
+
+deer = parse_input(input)
+puts(run_old_school_race(2503, deer).max_by { |v| v[1] })
+puts(run_new_race(2503, deer).max_by { |_, v| v })
